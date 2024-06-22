272f038874780a8a4c0edc204a329bcaa55e8b80
Edenys Denis González <holofist1@gmail.com>
2024-06-22 18:30:19 -0400

    remove preflight

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,9 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  corePlugins: {
+    preflight: false,
+  },
   theme: {
     extend: {},
   },
