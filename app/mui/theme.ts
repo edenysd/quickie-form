55ee1a50e9040866f08bbff145c2a7eff3508270
Edenys Denis González <holofist1@gmail.com>
2024-06-25 03:32:19 -0400

    add @mui/lab

--- app/mui/theme.ts
+++ app/mui/theme.ts
@@ -1,5 +1,6 @@
 import type { Theme, PaletteMode } from "@mui/material";
 import { createTheme, colors } from "@mui/material";
+import type {} from "@mui/lab/themeAugmentation";
 
 // Create a theme instance.
 const initTheme = ({
