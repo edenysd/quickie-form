bd40803ff5d96dee28901bf4aa51e611f0b5d8ea
Edenys Denis González <holofist1@gmail.com>
2024-06-26 01:53:18 -0400

    add reset password (in progress) route

---
+++ app/routes/reset-password/route.tsx
@@ -0,0 +1,34 @@
+import { Box, Button, Typography } from "@mui/material";
+import { type MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Reset Password" },
+    {
+      name: "description",
+      content:
+        "Do you forget your password? No problem, you can reset it here.",
+    },
+  ];
+};
+
+export default function NotFound404() {
+  return (
+    <Box
+      width={"100%"}
+      height={"100%"}
+      display={"flex"}
+      flexDirection={"column"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      gap={2}
+    >
+      <Typography variant="h2">In working progress</Typography>
+      <Typography variant="body1">Thank you for your patience</Typography>
+      <Link to="/">
+        <Button size="large">Go Back</Button>
+      </Link>
+    </Box>
+  );
+}
