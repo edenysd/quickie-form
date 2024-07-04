735df41d9012774d66bc63c6a30670901bbbd2c3
Edenys Denis González <holofist1@gmail.com>
2024-07-04 19:52:11 -0400

    add action to process user request to the bot and show it back

--- app/routes/home/route.tsx
+++ app/routes/home/route.tsx
@@ -1,12 +1,17 @@
 import { Box } from "@mui/material";
 import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
-import { redirect } from "@remix-run/react";
+import { json, redirect, useLoaderData } from "@remix-run/react";
 import { parse } from "@supabase/ssr";
 import supabaseServerClient from "~/supabase/supabaseServerClient";
 import AppAppBar from "./components/AppAppBar";
 import ChatBox from "./components/ChatBox";
 import { parseWithZod } from "@conform-to/zod";
 import z from "zod";
+import {
+  getChatSession,
+  getLastMessageFromCachedChatSession,
+} from "./bot/chat";
+import type { Content } from "@google/generative-ai";
 
 export const meta: MetaFunction = () => {
   return [
@@ -28,17 +33,38 @@ export async function action({ request }: LoaderFunctionArgs) {
 
   const supabase = supabaseServerClient(cookies, headers);
 
-  const { error } = await supabase.auth.getUser();
+  const {
+    error,
+    data: { user },
+  } = await supabase.auth.getUser();
 
-  if (error) {
+  if (error || user === null) {
     return redirect("/sign-in");
   }
 
   const formData = await request.formData();
   const data = parseWithZod(formData, { schema: formSchema });
-  console.log(data);
+  if (data.status !== "success") {
+    return data.reply();
+  }
+  const prompt = data.value.prompt;
 
-  return null;
+  const fetchHistory = async () => {
+    const response = await supabase
+      .from("Forms")
+      .select("history")
+      .eq("owner", user.id)
+      .limit(1)
+      .maybeSingle();
+
+    if (response.error) throw response.error;
+    return (response.data?.history || []) as Content[];
+  };
+
+  const chatSession = await getChatSession({ fetchHistory, id: "1" });
+  const result = await chatSession.sendMessage(prompt);
+
+  return json(result);
 }
 
 export async function loader({ request }: LoaderFunctionArgs) {
@@ -53,14 +79,20 @@ export async function loader({ request }: LoaderFunctionArgs) {
     return redirect("/sign-in");
   }
 
-  return null;
+  return json({
+    history: await getLastMessageFromCachedChatSession("1"),
+  });
 }
 
 export default function Home() {
+  const loaderData = useLoaderData<typeof loader>();
   return (
     <Box display={"flex"} flexDirection={"column"} alignItems={"center"}>
       <AppAppBar />
       <ChatBox />
+      <Box pt={10} pb={12}>
+        {loaderData.history ? JSON.stringify(loaderData.history) : null}
+      </Box>
     </Box>
   );
 }
