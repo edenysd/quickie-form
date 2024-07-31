c55a400cb8eab5da4804a0831904517df748fa25
Edenys Denis González <holofist1@gmail.com>
2024-07-30 22:57:44 -0400

    add virgil font has a font-face

--- app/mui/theme.ts
+++ app/mui/theme.ts
@@ -1,6 +1,6 @@
 import type { Theme, PaletteMode } from "@mui/material";
 import { createTheme, colors } from "@mui/material";
-import type {} from "@mui/lab/themeAugmentation";
+import Virgil from "./fonts/virgil.woff2"
 
 // Create a theme instance.
 const initTheme = ({
@@ -21,6 +21,19 @@ const initTheme = ({
         main: colors.red.A400,
       },
     },
+    components: {
+      MuiCssBaseline: {
+        styleOverrides: `
+          @font-face {
+            font-family: 'Virgil';
+            font-style: normal;
+            font-display: swap;
+            font-weight: 400;
+            src: local('Virgil'), local('Virgil-Regular'), url(${Virgil}) format('woff2');
+            unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+          }
+        `,
+      }
+    }
   });
-
 export default initTheme;
