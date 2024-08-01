708d34bef151f68aba506ba301028c4b88b77eed
Edenys Denis González <holofist1@gmail.com>
2024-07-31 21:56:55 -0400

    add some gap to App bar layout

--- app/components/AppAppBar.tsx
+++ app/components/AppAppBar.tsx
@@ -112,6 +112,7 @@ function AppAppBar({ children }: React.PropsWithChildren) {
               justifyContent={"space-between"}
               width={"100%"}
               height={"100%"}
+              gap={1}
             >
               <Box flexGrow={1}>{children}</Box>
               <Box
