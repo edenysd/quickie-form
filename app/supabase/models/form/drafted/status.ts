5c35ae6d783f125f110af7f9c731b585472ff36f
Edenys Denis González <holofist1@gmail.com>
2024-07-31 23:51:28 -0400

    rename module to status in order to manage all drafted status changes

--- app/supabase/models/form/drafted/status.ts
+++ app/supabase/models/form/drafted/status.ts
@@ -1,12 +1,33 @@
 import type { SupabaseClient, User } from "@supabase/supabase-js";
 
+export const resetDraftedForm = async ({
+  supabaseClient,
+  user,
+}: {
+  supabaseClient: SupabaseClient;
+  user: User;
+}) => {
+  const response = await supabaseClient
+    ?.from("Form_Templates")
+    .update([
+      {
+        history: null,
+        config: null,
+      },
+    ])
+    .eq("owner", user?.id)
+    .eq("status", "draft")
+    .select();
+  return response;
+};
+
 export const publishDraftedForm = async ({
   supabaseClient,
   templateName,
   user,
 }: {
   supabaseClient: SupabaseClient;
-  templateName: string,
+  templateName: string;
   user: User;
 }) => {
   const response = await supabaseClient
