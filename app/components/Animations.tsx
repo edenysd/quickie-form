ff8c07eef541c9e7062777a893e5117e0514aad9
Edenys Denis González <holofist1@gmail.com>
2024-08-02 12:02:34 -0400

    improve animation usage by allowing only coords params

--- app/components/Animations.tsx
+++ app/components/Animations.tsx
@@ -1,38 +1,26 @@
 import type { GrowProps } from "@mui/material";
 import { Grow } from "@mui/material";
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 export const TransitionGrowFromElementId = React.forwardRef(function Transition(
-  { targetElementId, ...props }: GrowProps & { targetElementId: string },
+  {
+    origin,
+    ...props
+  }: GrowProps & {
+    origin: {
+      x: number;
+      y: number;
+    } | null;
+  },
   ref
 ) {
-  const [origin, setOrigin] = useState<null | {
-    x: number;
-    y: number;
-  }>(null);
-
-  useEffect(() => {
-    const originElement = document.getElementById(targetElementId);
-    if (!originElement) return;
-
-    const originCoords = {
-      x:
-        originElement.getBoundingClientRect().x /
-          originElement.getBoundingClientRect().width || 0 * 100,
-      y:
-        (originElement.getBoundingClientRect().y /
-          originElement.getBoundingClientRect().height) *
-        100,
-    };
-    setOrigin(originCoords);
-  }, [targetElementId]);
-
   return (
     <Grow
       ref={ref}
       {...props}
       style={{
-        transformOrigin: origin ? `${origin.y}% ${origin.x}%` : "50% 50%",
+        ...props.style,
+        transformOrigin: origin ? `${origin.x}% ${origin.y}%` : "50% 50%",
       }}
     />
   );
