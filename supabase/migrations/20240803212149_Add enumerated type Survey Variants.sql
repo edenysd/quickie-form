e36c70982a2de0202cef51a438fcd38467cd8e71
Edenys Denis González <holofist1@gmail.com>
2024-08-03 18:28:54 -0400

    migration add enumerated type Survey Variants

---
+++ supabase/migrations/20240803212149_Add enumerated type Survey Variants.sql
@@ -0,0 +1,3 @@
+create type "public"."Survey Variants" as enum ('public_with_no_verification', 'public_with_single_use', 'public_with_email_verification', 'private_with_single_use', 'private_email_list');
+
+
