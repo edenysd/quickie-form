af4bcdc444dc46b2cb524d6e4fd96f13ebeb2970
Edenys Denis González <holofist1@gmail.com>
2024-08-03 16:58:42 -0400

    squash initial migrations on Form Templates table

--- supabase/migrations/20240803203404_Add new updated_at column to Form Templates.sql
+++
@@ -1,1 +0,0 @@
-alter table "public"."Form_Templates" add column "updated_at" timestamp with time zone default now();
--- supabase/migrations/20240803203552_Add new select allow policy for non owned form templates.sql
+++
@@ -1,6 +0,0 @@
-create policy "Enable select for authenticated users when has no owner"
-on "public"."Form_Templates"
-as permissive
-for select
-to authenticated
-using (((owner IS NULL) AND (status <> 'draft'::"Form Status")));
