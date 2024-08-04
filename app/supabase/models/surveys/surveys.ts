05d4b62f6328ce413697d9d36a39a74f5e147a65
Edenys Denis González <holofist1@gmail.com>
2024-08-04 10:34:16 -0400

    add surveys data layer module with ```getAllUserRunningSurveys``` and ```insertSurvey```

---
+++ app/supabase/models/surveys/surveys.ts
@@ -0,0 +1,48 @@
+import type { User } from "@supabase/supabase-js";
+import type {
+  MySupabaseClient,
+  SurveyVariant,
+} from "~/supabase/supabase.types";
+
+export const getAllUserRunningSurveys = async ({
+  supabaseClient,
+  user,
+}: {
+  supabaseClient: MySupabaseClient;
+  user: User;
+}) => {
+  const response = await supabaseClient
+    ?.from("Surveys")
+    .select()
+    .eq("survey_status", "open")
+    .order("created_at", { ascending: false })
+    .eq("owner", user?.id);
+  return response;
+};
+
+export const insertSurvey = async ({
+  surveyLabel,
+  templateId,
+  surveyVariant,
+  user,
+  supabaseClient,
+}: {
+  surveyLabel: string;
+  templateId: number;
+  surveyVariant: SurveyVariant;
+  user: User;
+  supabaseClient: MySupabaseClient;
+}) => {
+  const response = await supabaseClient
+    .from("Surveys")
+    .insert({
+      survey_label: surveyLabel,
+      template_id: templateId,
+      survey_variant: surveyVariant,
+      survey_status: "open",
+      owner: user.id,
+    })
+    .select();
+
+  return response;
+};
