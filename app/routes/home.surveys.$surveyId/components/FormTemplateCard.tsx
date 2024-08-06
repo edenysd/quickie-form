ff45ca13baacfb976a1b14330d4d5205af95f648
Edenys Denis González <holofist1@gmail.com>
2024-08-06 05:34:59 -0400

    remove unused code

--- app/routes/home.surveys.$surveyId/components/FormTemplateCard.tsx
+++ app/routes/home.surveys.$surveyId/components/FormTemplateCard.tsx
@@ -7,80 +7,14 @@ import {
   Grid,
   Typography,
 } from "@mui/material";
-import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
-import { json, redirect, useLoaderData } from "@remix-run/react";
-import { parse } from "@supabase/ssr";
-import supabaseServerClient from "~/supabase/supabaseServerClient";
-import { getSurveyById } from "~/supabase/models/surveys/surveys";
-import { getFormTemplateById } from "~/supabase/models/form-templates/forms";
+import { useLoaderData } from "@remix-run/react";
 import type { MouseEventHandler, ReactElement } from "react";
 import { useState } from "react";
 import { calculateOriginCoordsPercentageFromElement } from "~/components/Animations";
 import PreviewFormTemplateDialog from "~/components/PreviewFormTemplateDialog";
 import type { FormTemplateRow } from "~/supabase/supabase.types";
 import dayjs from "dayjs";
-
-export const meta: MetaFunction = () => {
-  return [
-    { title: "Survey Detail" },
-    {
-      name: "description",
-      content: "Details about a specific running survey",
-    },
-  ];
-};
-
-export async function action({ request }: LoaderFunctionArgs) {
-  const headers = new Headers();
-  const cookies = parse(request.headers.get("Cookie") ?? "");
-
-  const supabase = supabaseServerClient(cookies, headers);
-
-  const {
-    error,
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  if (error || user === null) {
-    return redirect("/sign-in");
-  }
-
-  const formData = await request.formData();
-  const _action = formData.get("_action") as string;
-
-  if (_action === null) {
-    return null;
-  }
-  return null;
-}
-
-export async function loader({ request, params }: LoaderFunctionArgs) {
-  const headers = new Headers();
-  const cookies = parse(request.headers.get("Cookie") ?? "");
-
-  const supabase = supabaseServerClient(cookies, headers);
-
-  const {
-    error,
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  if (error || !user) {
-    return redirect("/sign-in");
-  }
-
-  const surveyDetails = await getSurveyById({
-    supabaseClient: supabase,
-    surveyId: params.surveyId!,
-  });
-
-  const formTemplate = await getFormTemplateById({
-    supabaseClient: supabase,
-    templateId: surveyDetails.data!.template_id!.toString(),
-  });
-
-  return json({ surveyDetails, formTemplate, user });
-}
+import type { loader } from "../route";
 
 export default function FormTemplateCard() {
   const loaderData = useLoaderData<typeof loader>();
@@ -116,29 +50,27 @@ export default function FormTemplateCard() {
   return (
     <>
       {currentOverlayAction}
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={6}>
-          <Card>
-            <CardHeader
-              sx={{ overflow: "hidden" }}
-              title={loaderData.formTemplate.data?.name}
-              subheader="Template"
-            />
-            <CardContent sx={{ py: 0 }}>
-              <Typography variant="caption">
-                Last Update{" "}
-                {dayjs(loaderData.formTemplate.data!.updated_at!).format(
-                  "D/M/YYYY, h:mm:ss A"
-                )}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button onClick={handlePreview} color="info">
-                See preview
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
+      <Grid item xs={12} md={6}>
+        <Card>
+          <CardHeader
+            sx={{ overflow: "hidden" }}
+            title={loaderData.formTemplate.data?.name}
+            subheader="Template"
+          />
+          <CardContent sx={{ py: 0 }}>
+            <Typography variant="caption">
+              Last Update{" "}
+              {dayjs(loaderData.formTemplate.data!.updated_at!).format(
+                "D/M/YYYY, h:mm:ss A"
+              )}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button onClick={handlePreview} color="info">
+              See preview
+            </Button>
+          </CardActions>
+        </Card>
       </Grid>
     </>
   );
