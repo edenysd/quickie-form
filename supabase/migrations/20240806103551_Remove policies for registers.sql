95d94e10decab286048c8ab467ebf5c0e0403ac6
Edenys Denis González <holofist1@gmail.com>
2024-08-06 06:36:11 -0400

    remove policies

---
+++ supabase/migrations/20240806103551_Remove policies for registers.sql
@@ -0,0 +1,5 @@
+drop policy "Enable all for owner users of the responded survey" on "public"."Survey_Responses";
+
+drop policy "Enable all for owner users of the summarized survey" on "public"."Survey_Summaries";
+
+
