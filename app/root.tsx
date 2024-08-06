5d9eeb0fb7193e264a30cf4a5c4cdb80ac0b63db
Edenys Denis González <holofist1@gmail.com>
2024-08-06 08:08:02 -0400

    add new favicon

--- app/root.tsx
+++ app/root.tsx
@@ -33,6 +33,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/x-icon" href="/public/favicon.svg" />
         <Meta />
         <MuiMeta />
         <Links />
