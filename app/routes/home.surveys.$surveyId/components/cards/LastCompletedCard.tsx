33759fd483ef3b11dcabb3fa11bf8ff90e1e7fd1
Edenys Denis González <holofist1@gmail.com>
2024-08-08 18:16:48 -0400

    improve layout responsiveness for suvey details

--- app/routes/home.surveys.$surveyId/components/cards/LastCompletedCard.tsx
+++ app/routes/home.surveys.$surveyId/components/cards/LastCompletedCard.tsx
@@ -14,32 +14,30 @@ import dayjs from "dayjs";
 export default function LastCompletedCard() {
   const loaderData = useLoaderData<typeof loader>();
   return (
-    <>
-      {loaderData.surveySummary.data !== null ? (
-        <Grid item md={3} xs={12} sm={6}>
-          <Card>
-            <CardHeader
-              sx={{ overflow: "hidden", textAlign: "center" }}
-              title={"Last Completed"}
-            />
-            <CardContent sx={{ py: 0 }}>
-              <Box
-                display={"flex"}
-                alignItems={"center"}
-                justifyContent={"center"}
-                gap={0.5}
-              >
-                <Typography variant="body1">
-                  {dayjs(loaderData.formTemplate.data!.updated_at!).format(
+    <Grid item md={3} xs={6}>
+      <Card>
+        <CardHeader
+          sx={{ overflow: "hidden", textAlign: "center" }}
+          title={"Last Completed"}
+        />
+        <CardContent sx={{ py: 0 }}>
+          <Box
+            display={"flex"}
+            alignItems={"center"}
+            justifyContent={"center"}
+            gap={0.5}
+          >
+            <Typography variant="body1">
+              {loaderData.surveySummary.data?.updated_at
+                ? dayjs(loaderData.surveySummary.data?.updated_at).format(
                     "D/M/YYYY, h:mm:ss A"
-                  )}
-                </Typography>
-              </Box>
-            </CardContent>
-            <CardActions></CardActions>
-          </Card>
-        </Grid>
-      ) : null}
-    </>
+                  )
+                : "-"}
+            </Typography>
+          </Box>
+        </CardContent>
+        <CardActions></CardActions>
+      </Card>
+    </Grid>
   );
 }
--- app/routes/home.surveys.$surveyId/components/cards/TotalSurveyCard.tsx
+++ app/routes/home.surveys.$surveyId/components/cards/TotalSurveyCard.tsx
@@ -14,31 +14,27 @@ import { PeopleRounded } from "@mui/icons-material";
 export default function TotalSurveyCard() {
   const loaderData = useLoaderData<typeof loader>();
   return (
-    <>
-      {loaderData.surveySummary.data !== null ? (
-        <Grid item md={3} xs={12} sm={6}>
-          <Card>
-            <CardHeader
-              sx={{ overflow: "hidden", textAlign: "center" }}
-              title={"Total Completed"}
-            />
-            <CardContent sx={{ py: 0 }}>
-              <Box
-                display={"flex"}
-                alignItems={"center"}
-                justifyContent={"center"}
-                gap={0.5}
-              >
-                <PeopleRounded fontSize="large" />
-                <Typography variant="body1" fontSize={25} fontFamily={"Virgil"}>
-                  {loaderData.surveySummary.data?.total_entries}
-                </Typography>
-              </Box>
-            </CardContent>
-            <CardActions></CardActions>
-          </Card>
-        </Grid>
-      ) : null}
-    </>
+    <Grid item md={3} xs={6}>
+      <Card>
+        <CardHeader
+          sx={{ overflow: "hidden", textAlign: "center" }}
+          title={"Total Completed"}
+        />
+        <CardContent sx={{ py: 0 }}>
+          <Box
+            display={"flex"}
+            alignItems={"center"}
+            justifyContent={"center"}
+            gap={0.5}
+          >
+            <PeopleRounded fontSize="large" />
+            <Typography variant="body1" fontSize={25} fontFamily={"Virgil"}>
+              {loaderData.surveySummary.data?.total_entries || 0}
+            </Typography>
+          </Box>
+        </CardContent>
+        <CardActions></CardActions>
+      </Card>
+    </Grid>
   );
 }
