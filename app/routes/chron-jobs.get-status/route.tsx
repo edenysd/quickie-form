42eb9435176d53875847c29229561b7ed433245e
Edenys Denis González <holofist1@gmail.com>
2025-02-26 01:42:45 -0500

    Add 3 retries and change second chron job schedule

--- app/routes/chron-jobs.get-status/route.tsx
+++ app/routes/chron-jobs.get-status/route.tsx
@@ -1,16 +1,24 @@
-import type { LoaderFunctionArgs } from "@vercel/remix";
-import { json } from "@remix-run/react";
-import { getTotalRunningSurveys } from "~/supabase/models/surveys/surveys";
-import supabasePrivateServerClient from "~/supabase/supabasePrivateServerClient";
+import type {LoaderFunctionArgs} from '@vercel/remix';
+import {json} from '@remix-run/react';
+import {getTotalRunningSurveys} from '~/supabase/models/surveys/surveys';
+import supabasePrivateServerClient from '~/supabase/supabasePrivateServerClient';
 
-export async function loader({ ...args }: LoaderFunctionArgs) {
+export async function loader({...args}: LoaderFunctionArgs) {
   const privateSupabase = supabasePrivateServerClient();
 
-  const totalRunningSurveys = await getTotalRunningSurveys({
-    supabaseClient: privateSupabase,
+  let totalRunningSurveys = await getTotalRunningSurveys({
+    supabaseClient: privateSupabase
+  });
+
+  totalRunningSurveys = await getTotalRunningSurveys({
+    supabaseClient: privateSupabase
+  });
+
+  totalRunningSurveys = await getTotalRunningSurveys({
+    supabaseClient: privateSupabase
   });
 
   return json({
-    totalRunningSurveys: totalRunningSurveys.count,
+    totalRunningSurveys: totalRunningSurveys.count
   });
 }
